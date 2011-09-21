ad5eaab30be9629ea7e76523309def0c3672bc59
Gene <genewoncook@unknowne4ce8f1a88b8.gateway.2wire.net>
2011-09-21 12:14:09 -0500

    Initial Commit

---
+++ Sandals/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  Sandals
+#
+#  Created by Gene Cook on 9/21/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
